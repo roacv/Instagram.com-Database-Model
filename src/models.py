ab3de6acd2d59528164cb05313937f14e2202870
roacv <roacv@hotmail.com>
2023-03-28 16:57:58 +0000

    Model

--- src/models.py
+++ src/models.py
@@ -8,26 +8,38 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    url = Column(String(250), nullable=False)
+    description = Column(String(500), nullable=False)
+    user = relationship('User', back_populates='posts')
 
-    def to_dict(self):
-        return {}
+class Like(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'),nullable=False)
+    description = Column(String(500), nullable=False)
+    user = relationship('User', back_populates='likes')
+    post = relationship('Post', back_populates='likes')
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+    user_follower = Column(Integer, ForeignKey('users.id'),nullable=False)
+    user = relationship('User', back_populates='followers')
+
 
 ## Draw from SQLAlchemy base
 try:
